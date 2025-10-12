cc221ceb60d377037be426149fec5ae955bea256
Kutubuddin-Rasel <raselvai257@gmail.com>
2025-10-12 15:55:58 +0600

    update middleware for checking if user does not have any token.

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,29 @@ import { getToken } from "next-auth/jwt"
 export { default } from "next-auth/middleware"
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({req:request})
-    const url = request.nextUrl;
+  const token = await getToken({ req: request })
+  const url = request.nextUrl;
 
-    if(token && (
-        url.pathname.startsWith('/sign-in') ||
-        url.pathname.startsWith('/sign-up') ||
-        url.pathname.startsWith('/verify')  ||
-        url.pathname.startsWith('/')
-    )){
-        return NextResponse.redirect(new URL('/dashboard', request.url))
-     }
-
-  return NextResponse.redirect(new URL('/home', request.url))
+  if (token && (
+    url.pathname.startsWith('/sign-in') ||
+    url.pathname.startsWith('/sign-up') ||
+    url.pathname.startsWith('/verify') ||
+    url.pathname.startsWith('/')
+  )) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+  if (!token && (url.pathname.startsWith('/dashboard'))) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+  return NextResponse.next();
 }
- 
+
 export const config = {
-  matcher:[
+  matcher: [
     '/',
     '/sign-in',
     '/sing-up',
     '/dashboard/:path*',
     '/verify/:path*'
-    ]
+  ]
 }
